89bb8e18eeb801a991771d2067bb75bc963ce3f7
kaioyuri96 <kaioyuri96@gmail.com>
2024-07-17 23:23:09 +0000

    Mudança do inglês para o português

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import { cookies } from 'next/headers'
+
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">Meu portifolio</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+         Bem vindo ao meu portifólio.
       </p>
     </section>
   );
 }
+
+
