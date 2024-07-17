5c3950774d67f3c64bd3bb1db1ae86bc90e9a2ce
kaioyuri96 <kaioyuri96@gmail.com>
2024-07-17 23:23:00 +0000

    Mudança do inglês para o português

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,10 @@ export default function NotFound() {
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
-        Oh no! This page doesn't exist.
+        Ah não! Esta página não existe.
       </h1>
       <p>
-        If you expected to see something here, let me know (lee@vercel.com).
+        Se você esperava ver alguma coisa aqui, deixe-me saber (kaioyuri119@gmail.com).
       </p>
     </section>
   );
