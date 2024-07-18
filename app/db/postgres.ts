0871796ee0046e4a3caa5e9c08511907c22027a9
kaioyuri96 <kaioyuri96@gmail.com>
2024-07-18 01:02:34 +0000

    revert: conf ssl tem relação com o erro de conexão com o banco de dados

--- app/db/postgres.ts
+++ app/db/postgres.ts
@@ -1,7 +1,5 @@
 import postgres from 'postgres';
 
 export const sql = postgres(process.env.POSTGRES_URL!, {
-  ssl: {
-    rejectUnauthorized: false, // Exemplo de configuração específica do SSL
-  },
+  ssl: 'allow',
 });
