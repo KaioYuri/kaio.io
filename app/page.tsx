f060f43cc78b384e88e575961d8f2b67f87e2115
kaioyuri96 <kaioyuri96@gmail.com>
2024-07-19 19:30:39 +0000

    atualização de página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -64,10 +64,7 @@ async function Views({ slug }: { slug: string }) {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">Meu portifolio</h1>
-      <p className="prose prose-neutral dark:prose-invert">
-         Oi, meu nome é Kaio Yuri 👋
-      </p>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">oi, sou kaio yuri 👋</h1>
       <p className="prose prose-neutral dark:prose-invert">
          Bem vindo ao meu portifólio.
       </p>
