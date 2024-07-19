336d4fb3965211fa3b7616bdab6c3d6e0face31f
kaioyuri96 <kaioyuri96@gmail.com>
2024-07-19 18:58:34 +0000

    feat: funcionalidade de link para blog, ArrowIcon e badge.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,65 @@
 import { cookies } from 'next/headers'
+import { Suspense } from 'react';
+import { unstable_noStore as noStore } from 'next/cache';
+import Link from 'next/link';
+import Image from 'next/image';
+import ViewCounter from 'app/blog/view-counter';
+import { getViewsCount } from './db/queries';
+
+function Badge(props) {
+  return (
+    <a
+      {...props}
+      target="_blank"
+      className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+    />
+  );
+}
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+function BlogLink({ slug, name }) {
+  return (
+    <div className="group">
+      <a
+        href={`/blog/${slug}`}
+        className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800"
+      >
+        <div className="flex flex-col">
+          <p className="font-medium text-neutral-900 dark:text-neutral-100">
+            {name}
+          </p>
+          <Suspense fallback={<p className="h-6" />}>
+            <Views slug={slug} />
+          </Suspense>
+        </div>
+        <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
+          <ArrowIcon />
+        </div>
+      </a>
+    </div>
+  );
+}
+
+async function Views({ slug }: { slug: string }) {
+  let views = await getViewsCount();
+  return <ViewCounter allViews={views} slug={slug} />;
+}
 
 export default function Page() {
   return (
