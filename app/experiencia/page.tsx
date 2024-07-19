09fdeff2a723616e5a70c6c2cf0d62adf97dace7
kaioyuri96 <kaioyuri96@gmail.com>
2024-07-19 20:27:47 +0000

    Mudança na pagina de experiencia

---
+++ app/experiencia/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">Minha Jornada 🛣️</h1>
+      <p className="prose prose-neutral dark:prose-invert">
+        Ainda em manutenção 👷‍♂️.
+      </p>
+    </section>
+  );
+}
