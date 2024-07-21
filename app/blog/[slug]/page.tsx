b13ce3881c35c7cea8f2733cc502a75090aef302
kaioyuri96 <kaioyuri96@gmail.com>
2024-07-21 15:47:44 +0000

    feat: Adicionado sumário aos posts do blog

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -128,6 +128,9 @@ export default function Blog({ params }) {
           <Views slug={post.slug} />
         </Suspense>
       </div>
+        <Suspense>
+        {post.metadata.summary}
+        </Suspense>
       <article className="prose prose-quoteless prose-neutral dark:prose-invert">
         <CustomMDX source={post.content} />
       </article>
