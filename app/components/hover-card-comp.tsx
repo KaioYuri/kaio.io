4755b9327746806bd9f4de7a4104a3aa7faabd27
kaioyuri96 <kaioyuri96@gmail.com>
2024-08-09 19:38:32 +0000

    estilização para forçar a centralização do hover card

--- app/components/hover-card-comp.tsx
+++ app/components/hover-card-comp.tsx
@@ -19,32 +19,32 @@ interface HoverCardProps {
     data: string;
   }
  
-export function HoverCardFollow({ avatar, arroba, descricao, data }: HoverCardProps) {
-  return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <Button variant="link">@{arroba}</Button>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex justify-between space-x-4">
-          <Avatar>
-            <AvatarImage src={avatar} />
-            <AvatarFallback>VC</AvatarFallback>
-          </Avatar>
-          <div className="space-y-1">
-            <h4 className="text-sm font-semibold">@{arroba}</h4>
-            <p className="text-sm">
-              {descricao}
-            </p>
-            <div className="flex items-center pt-2">
-              <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
-              <span className="text-xs text-muted-foreground">
-                {data}
-              </span>
+  export function HoverCardFollow({avatar, arroba, descricao, data }: HoverCardProps) {
+    return (
+      <HoverCard>
+        <HoverCardTrigger asChild>
+          <Button className="p-0" variant="link">@{arroba}</Button>
+        </HoverCardTrigger>
+        <HoverCardContent className="w-80 bg-black text-left inline-block">
+          <div className="flex justify-between space-x-4">
+            <Avatar>
+              <AvatarImage src={avatar} />
+              <AvatarFallback>VC</AvatarFallback>
+            </Avatar>
+            <div className="space-y-1">
+              <h4 className="text-sm font-semibold">@{arroba}</h4>
+              <p className="text-sm">
+                {descricao}
+              </p>
+              <div className="flex items-center pt-2">
+                <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                <span className="text-xs text-muted-foreground">
+                  {data}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      </HoverCardContent>
-    </HoverCard>
-  )
-}
+        </HoverCardContent>
+      </HoverCard>
+    )
+  }
